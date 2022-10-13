20fb8fd6d36ba2c305e55915e6941c0a65111e28
Mingyi Zhou <78472483+AnonymousAuthor000@users.noreply.github.com>
2022-10-13 15:59:51 +1100

    optimize the extra op

--- tfl_source_file/ObfOptions.cc
+++ tfl_source_file/ObfOptions.cc
@@ -105,14 +105,14 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TfLiteTensor* output;
   TF_LITE_ENSURE_OK(context,
                     GetOutputSafe(context, node, kOutputTensor, &output));
-  if (output->type != kTfLiteString) {
-    // if (IsConstantTensor(GetInput(context, node, kShapeTensor))) {
-    //   TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
-    // } else {
-    //   SetTensorToDynamic(output);
-    // }
-    TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
-  }
+  // if (output->type != kTfLiteString) {
+  //   // if (IsConstantTensor(GetInput(context, node, kShapeTensor))) {
+  //   //   TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
+  //   // } else {
+  //   //   SetTensorToDynamic(output);
+  //   // }
+  //   TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
+  // }
   return kTfLiteOk;
 }
 
@@ -126,9 +126,9 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   // There are two ways in which the 'output' can be made dynamic: it could be
   // a string tensor, or its shape cannot be calculated during Prepare(). In
   // either case, we now have all the information to calculate its shape.
-  if (IsDynamicTensor(output)) {
-    TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
-  }
+  // if (IsDynamicTensor(output)) {
+  //   TF_LITE_ENSURE_OK(context, ResizeOutput(context, node));
+  // }
 
   // Note that string tensors are always "dynamic" in the sense that their size
   // is not known until we have all the content. This applies even when their
@@ -136,13 +136,13 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   // any memory by ResizeOutput(), and we need to do it manually here. Since
   // reshape doesn't change the data, the output tensor needs exactly as many
   // bytes as the input tensor.
-  if (output->type == kTfLiteString) {
-    auto bytes_required = input->bytes;
-    TfLiteTensorRealloc(bytes_required, output);
-    output->bytes = bytes_required;
-  }
-
-  memcpy(output->data.raw, input->data.raw, input->bytes);
+  // if (output->type == kTfLiteString) {
+  //   auto bytes_required = input->bytes;
+  //   TfLiteTensorRealloc(bytes_required, output);
+  //   output->bytes = bytes_required;
+  // }
+  // output = input;
+  // memcpy(output->data.raw, input->data.raw, input->bytes);
 
   return kTfLiteOk;
 }
