e9ac5a2c89ba6e0cc5c6aa15f561c519b5e968ca
Mingyi Zhou <78472483+AnonymousAuthor000@users.noreply.github.com>
2022-10-14 13:46:10 +1100

    Update build_obf.sh

--- build_obf.sh
+++ build_obf.sh
@@ -1,4 +1,4 @@
 python obfuscation.py --model_name=squeezenet --extra_layer=30 --shortcut=30
 pip uninstall -y tensorflow
 pip install /tmp/tensorflow_pkg/tensorflow-2.9.1-cp38-cp38-linux_x86_64.whl
-python test_obf.py --model_name=squeezenet
+python test_model.py --model_name=squeezenet
